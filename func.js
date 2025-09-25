2716013a6baa9a81821ee609c88fd0ab1498c6dd
khoidevcomer <oopsrex058@gmail.com>
2025-09-25 20:38:19 +0700

    Update func.js

--- func.js
+++ func.js
@@ -5,17 +5,21 @@
     const navbarList = document.getElementById('navbarList');
     navbarList.classList.toggle('open');
         });
+document.getElementById('playScrem').addEventListener('click', function() {
+  document.getElementById('myAudio').play();
+});
          /*document.getElementById('colorButton').addEventListener('click', function() {
             const colors = ['#FF5733', '#33FF57', '#3357FF', '#F1C40F', '#8E44AD'];
             const randomColor = colors[Math.floor(Math.random() * colors.length)];
             document.body.style.backgroundColor = randomColor; // This will change the background color, not the image
         });*/
         
-    function playSound() {
+/*    function playSound() {
         const audio = new Audio('videoplayback.mp3');
         audio.play();
     }
     const play = document.querySelector('#playScrem');
     play.onclick = () => {
         playSound();
-    }
+    }*/
+
